998c9f55455206da7087c2181f5f520a8b40692a
Rahul <Rahul.Jaiswal@bentley.com>
2024-11-19 09:17:11 +0530

    added sentry

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,23 @@ import App from "./App.jsx";
 import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { shadesOfPurple } from "@clerk/themes";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://beb2fe4c6b6aaea15eae7d50a7dee296@o4508322765471744.ingest.de.sentry.io/4508322775367760",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", "https://rahuljobportal.z29.web.core.windows.net"],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
