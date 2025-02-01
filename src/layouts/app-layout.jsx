fca98fdb71ea2ce78d9c8d1ca9ceb534efef0969
Rahul <Rahul.Jaiswal@bentley.com>
2025-02-01 13:29:03 +0530

    fix css

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -5,10 +5,10 @@ const AppLayout = () => {
   
   return (
     <div>
-      <div className="grid-background"></div>
       <main className="main-screen container" style={{backgroundColor : "white"}}>
         <Header />
-        <Outlet />
+        <div style={{height: "calc(100% - 180px)", overflowY: "scroll", paddingBottom: "20px"}} className="content"><Outlet /></div>
+        <div style={{position: "absolute", bottom: "0px",overflowX: "hidden", left: "0px",right: "0px",  height: "80px", backgroundColor: "black", width: '100%', color: "white", display: "flex", justifyContent: "center", alignItems: "center"}}>Made by Rahul</div>
       </main>
     </div>
   );
